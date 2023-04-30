b52b45850662919735ef8be5de0782ebd1305c85
xiao <yxmaker@hotmail.com>
2023-04-30 23:14:46 +0800

    提交

--- src/decorator/wear/Person.java
+++ src/decorator/wear/Person.java
@@ -1,12 +1,13 @@
 package decorator.wear;
 
 public class Person implements ICharacter{
-    @Override
+
     private String name;
     public Person (String name){
         this.name = name;
     }
+    @Override
     public void show() {
-
+        System.out.println("decorate " + name);
     }
 }
